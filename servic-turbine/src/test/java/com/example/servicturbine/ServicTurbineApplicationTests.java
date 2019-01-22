1fc52d260b6c75b47caa4931f7f5033a131103ba
wulei <xuxx5@asiainfo.com>
2019-01-22 10:50:49 +0800

    新增servic-turbine

---
+++ servic-turbine/src/test/java/com/example/servicturbine/ServicTurbineApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.servicturbine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServicTurbineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
