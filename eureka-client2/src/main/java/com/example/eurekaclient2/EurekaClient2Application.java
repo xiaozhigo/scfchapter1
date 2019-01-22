21cb4abe9e2851a3505196e3c848c2cc8eda12b2
wulei <xuxx5@asiainfo.com>
2019-01-22 10:48:09 +0800

    新增servic-turbine

--- eureka-client2/src/main/java/com/example/eurekaclient2/EurekaClient2Application.java
+++ eureka-client2/src/main/java/com/example/eurekaclient2/EurekaClient2Application.java
@@ -2,8 +2,18 @@ package com.example.eurekaclient2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class EurekaClient2Application {
 
 	public static void main(String[] args) {
--- eureka-client2/src/main/java/com/example/eurekaclient2/controller/ClientController.java
+++ eureka-client2/src/main/java/com/example/eurekaclient2/controller/ClientController.java
@@ -1,7 +1,9 @@
 package com.example.eurekaclient2.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,5 +19,15 @@ public class ClientController {
     @RequestMapping("/port")
     public String port(){
         return "port:"+port+"正在启动中";
+    }
+
+    @RequestMapping("/home")
+    @HystrixCommand(fallbackMethod = "homeError")
+    public String home(@RequestParam String name){
+        return "welcome to home "+name+",port:"+port;
+    }
+
+    public String homeError(String name){
+        return "sorry "+name+" i can't help you";
     }
 }
